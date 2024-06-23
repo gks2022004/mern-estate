bef62dfc3dcf4a925be1a22b25d7008173f86962
gks2022004 <imgauravkumar7@gmail.com>
2024-06-23 11:13:01 +0530

    add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About';
 export default function App() {
   return (
-    <div>
-      <h1 className='text-3xl font-bold text-red-600'>Hello, World!</h1>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/SignIn' element={<SignIn />} />
+    <Route path='/SignOut' element={<SignOut />} />
+    <Route path='/Profile' element={<Profile />} />
+    <Route path='/About' element={<About />} />
+
+   </Routes>
+   
+   </BrowserRouter>
   )
 }
---
+++ src/pages/SignOut.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SignOut() {
+  return (
+    <div>
+      {/* Use React's functionality here */}
+      <h1>Hello, React!</h1>
+    </div>
+  )
+}
